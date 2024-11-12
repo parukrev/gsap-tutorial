9360c2b60d841cc2ceb8fd061f9d8fffd3997d52
parukrev <xavierrevian@gmail.com>
2024-11-12 14:11:18 +0700

    initial commit

--- src/pages/GsapTimeline.jsx
+++ src/pages/GsapTimeline.jsx
@@ -17,22 +17,38 @@ const GsapTimeline = () => {
       ease: "power2.inOut",
     });
 
-    timeline.to('#blue-box', {
+    timeline.to("#blue-box", {
+      y: 350,
+      scale: 1.5,
+      rotation: 180,
+      borderRadius: "50%",
+      duration: 2,
+      ease: "back.inOut",
+    });
+
+    timeline.to("#blue-box", {
       x: 500,
       scale: 1,
       rotation: 360,
-      borderRadius: '20px',
+      borderRadius: "20px",
       duration: 2,
-      ease: "back.inOut"
-    })
+      ease: "back.inOut",
+    });
   }, []);
+
+  const condition = () => {
+    timeline.paused() ? timeline.play() : timeline.pause();
+  };
 
   return (
     <main>
       <div className="">
         <div id="blue-box" className="w-20 h-20 bg-blue-500 rounded-lg" />
       </div>
-      <button id="btn" className="bg-red-800 px-3 py-2 w-max mt-10 rounded-md">
+      <button
+        onClick={condition}
+        className="bg-red-800 px-3 py-2 w-max mt-10 rounded-md"
+      >
         Play/Pause
       </button>
     </main>
